e13aae05a2f0191ccd44b4c056620a54bcbe08b1
zch <zhangchenghui.dev@gmail.com>
2018-10-09 10:57:00 +0800

    更新测试单元

--- client_test.go
+++ client_test.go
@@ -1,1 +1,15 @@
 package wxpay
+
+import "testing"
+
+func TestClient_UnifiedOrder(t *testing.T) {
+	client := NewClient(NewAccount("xxxxx", "xxx", "xxxxx", false))
+	params := make(Params)
+	params.SetString("body", "test").
+		SetString("out_trade_no", "58867657575757").
+		SetInt64("total_fee", 1).
+		SetString("spbill_create_ip", "127.0.0.1").
+		SetString("notify_url", "http://notify.objcoding.com/notify").
+		SetString("trade_type", "APP")
+	t.Log(client.UnifiedOrder(params))
+}
